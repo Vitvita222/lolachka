782a5c7c0445235184f8e708e9906386e7ed9878
Vitvita222 <115868179+Vitvita222@users.noreply.github.com>
2022-11-04 21:32:37 +0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+let symbol = document.querySelector('.input3')
+
+
+
+function sum() {
+    var n1 , n2 , res;
+    n1=Number(document.querySelector('.input1').value);
+    n2=Number(document.querySelector('.input2').value);
+    res=n1 + n2;
+    document.querySelector('.jami1').value=res;
+}
+
+
