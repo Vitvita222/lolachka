cd61064ba674b647a6b1ad12f1b5bd7b20f97d9a
Vitvita222 <115868179+Vitvita222@users.noreply.github.com>
2022-11-05 20:05:53 +0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,13 +1,19 @@
 let symbol = document.querySelector('.input3')
 
-
-
-function sum() {
+function mimateba() {
     var n1 , n2 , res;
     n1=Number(document.querySelector('.input1').value);
     n2=Number(document.querySelector('.input2').value);
     res=n1 + n2;
     document.querySelector('.jami1').value=res;
+}
+
+function gamokleba() {
+    var n1 , n2 , res;
+    n1=Number(document.querySelector('.input1-1').value);
+    n2=Number(document.querySelector('.input2-1').value);
+    res=n1 - n2;
+    document.querySelector('.sxvaoba').value=res;
 }
 
 
